0a18e4236dd235356909907578833e8abdde71a9
Think <think@Thinks-MacBook-Air.local>
2017-06-14 23:38:38 -0400

    adjusted tests

--- src/ScoreTable.test.js
+++ src/ScoreTable.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import ScoreTable from './ScoreTable';
+import ScoreTable from '../ScoreTable';
 
 describe('<ScoreTable/>', () => {
     it('Renders without crashing', () => {
