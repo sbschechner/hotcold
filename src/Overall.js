e661714a6fedd31577dcd418f18dd25741dd0e99
Think <think@Thinks-MacBook-Air.local>
2017-06-15 23:47:56 -0400

    created splash component

--- src/Overall.js
+++ src/Overall.js
@@ -4,11 +4,46 @@ import './overall.css';
 
 
 class Overall extends Component {
-  render() {
-    return (
+    constructor(props){
+    super(props);
+    this.state = {
+        showSplash : true,
+        };
+
+this.changeSplash = this.changeSplash.bind(this)
+  }
+
+changeSplash(){
+  this.setState({showSplash : false})
+}
+
+
+ splash(){ //should run the conditional formatting 
+    if(this.state.showSplash === true){
+      return (
+      <div className="splashPage">
+        <h1>Hot and Cold Game</h1>
+        <button id="playBut" onClick={this.changeSplash}> Play</button>
+      </div>
+      )
+    }
+
+    else { //
+     return( 
       <div className = "overallCont">
         <h1> Hot and Cold App </h1>
         <GuessInput /> 
+      </div>
+      )
+    }
+  } 
+
+
+
+  render() {
+    return (
+      <div className= "splashDecider" >
+        {this.splash()}  
       </div>
     );
   }
